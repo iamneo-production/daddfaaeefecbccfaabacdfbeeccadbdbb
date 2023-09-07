b8fbbe42156f6f2c7c7aa449c882d1859fc7d94d
Iamneo <techadmin@iamneo.ai>
2023-09-07 16:19:19 +0000

    Backup Commit

--- src/main/test/java/pendrive_stepdenition.java
+++ src/main/test/java/pendrive_stepdenition.java
@@ -1,3 +1,26 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import static org.junit.Assert.assertEquals;
 public class pendrive_stepdenition {
-    
+    private int cartQuantity = 0; // This variable will store the quantity in the cart
+
+    @Given("User searches for HP Pen Drive")
+    public void userSearchesForHPPenDrive() {
+        // Your code to navigate to the product page and search for HP Pen Drive
+        // This step may involve opening a browser and performing a search.
+    }
+
+    @When("Add the first result on the page with quantity {int}")
+    public void addPenDriveToCart(int quantity) {
+        // Your code to add the product to the cart with the specified quantity
+        // This step may involve clicking buttons, filling out forms, etc.
+        cartQuantity = quantity; // Update the cart quantity
+    }
+
+    @Then("Cart should display {int} pen drive")
+    public void verifyCartQuantity(int expectedQuantity) {
+        // Your code to verify the quantity in the cart matches the expected quantity
+        assertEquals(expectedQuantity, cartQuantity);
+    }
 }
